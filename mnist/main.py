9964d6bd97a93420f101ebcdc40f8bd540930956
Angela Jiang <to.ajiang@gmail.com>
2018-09-18 14:38:36 -0400

    Add batch statistics to mnist

--- mnist/main.py
+++ mnist/main.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import argparse
+import pickle
+import os
 import time
 import torch
 import torch.nn as nn
@@ -26,52 +28,128 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-def train(args, model, device, train_loader, optimizer, epoch):
+
+def get_stat(data):
+    # TODO: Add num backpropped
+    stat = {}
+    stat["average"] = np.average(data)
+    stat["p25"] = np.percentile(data, 25)
+    stat["p50"] = np.percentile(data, 50)
+    stat["p75"] = np.percentile(data, 75)
+    stat["p90"] = np.percentile(data, 90)
+    stat["max"] = max(data)
+    stat["min"] = min(data)
+    return stat
+
+
+def update_batch_stats(batch_stats, num_backpropped, pool_losses=None, chosen_losses=None, gradients=None):
+    '''
+    batch_stats = [{'chosen_losses': {stat},
+                   'pool_losses': {stat}}]
+    '''
+    snapshot = {"chosen_losses": get_stat(chosen_losses),
+                "pool_losses": get_stat(pool_losses)}
+    batch_stats.append(snapshot)
+
+
+def train(args,
+          model,
+          device,
+          trainloader,
+          optimizer,
+          epoch,
+          total_num_images_backpropped,
+          images_hist,
+          batch_stats=None):
+
+    print('\nEpoch: %d' % epoch)
     model.train()
-    all_losses = []
+    train_loss = 0
+    correct = 0
+    total = 0
+
+    losses_pool = []
+    data_pool = []
+    targets_pool = []
+    ids_pool = []
+    num_backprop = 0
     loss_reduction = None
 
-    train_loss = 0
-    num_backprop = 0
+    for batch_idx, (data, targets, image_id) in enumerate(trainloader):
+        data, targets = data.to(device), targets.to(device)
 
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
         if args.selective_backprop:
-            loss = F.nll_loss(output, target, reduce=False)
-            k = min(len(loss), args.top_k)
-            values, indices = torch.topk(loss, k)
-            if len(all_losses) == len(loss):
-                all_losses_tensor = torch.cat(all_losses)
-                loss_reduction = torch.mean(all_losses_tensor)
+
+            output = model(data)
+            loss = F.nll_loss(output, targets)
+            losses_pool.append(loss.item())
+            data_pool.append(data)
+            targets_pool.append(targets)
+            ids_pool.append(image_id.item())
+
+            if len(losses_pool) == args.pool_size:
+            # Choose frames from pool to backprop
+                indices = np.array(losses_pool).argsort()[-args.top_k:]
+                chosen_data = [data_pool[i] for i in indices]
+                chosen_targets = [targets_pool[i] for i in indices]
+                chosen_ids = [ids_pool[i] for i in indices]
+                chosen_losses = [losses_pool[i] for i in indices]
+
+                data_batch = torch.stack(chosen_data, dim=1)[0]
+                targets_batch = torch.cat(chosen_targets)
+                output_batch = model(data_batch) # redundant
+
+                for chosen_id in chosen_ids:
+                    images_hist[chosen_id] += 1
+
+                # Get stats for batches
+                if batch_stats is not None:
+                    update_batch_stats(batch_stats,
+                                       total_num_images_backpropped,
+                                       pool_losses = losses_pool, 
+                                       chosen_losses = chosen_losses)
+
+
+                # Note: This will only work for batch size of 1
+                loss_reduction = F.nll_loss(output_batch, targets_batch)
+                optimizer.zero_grad()
                 loss_reduction.backward()
                 optimizer.step()
-                all_losses = []
                 train_loss += loss_reduction.item()
-                num_backprop += 1
-            else:
-                all_losses.append(values)
+                num_backprop += args.top_k
+
+                losses_pool = []
+                data_pool = []
+                targets_pool = []
+                ids_pool = []
+
+                output = output_batch
+                targets = targets_batch
+
         else:
-            loss_reduction = F.nll_loss(output, target)
+            output = net(data)
+            loss_reduction = F.nll_loss(output, targets)
+            optimizer.zero_grad()
             loss_reduction.backward()
             optimizer.step()
             train_loss += loss_reduction.item()
-            num_backprop += 1
+            num_backprop += args.batch_size
+
+        _, predicted = output.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
         if batch_idx % args.log_interval == 0 and loss_reduction is not None:
-            print('train_debug,{},{},{:.6f},{:.6f},{}'.format(
+            print('train_debug,{},{},{:.6f},{:.6f},{},{:.6f}'.format(
                         epoch,
-                        num_backprop,
+                        total_num_images_backpropped + num_backprop,
                         loss_reduction.item(),
                         train_loss / float(num_backprop),
-                        time.time()))
-            '''
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss_reduction.item()))
-            '''
+                        time.time(),
+                        100.*correct/total))
+    return num_backprop
 
-def test(args, model, device, test_loader, epoch):
+def test(args, model, device, test_loader, epoch, total_num_images_backpropped):
     model.eval()
     test_loss = 0
     correct = 0
@@ -86,28 +164,25 @@ def test(args, model, device, test_loader, epoch):
             total += target.size(0)
 
     test_loss /= len(test_loader.dataset)
-    print('test_debug,{},{:.6f},{:.6f},{}'.format(
+    print('test_debug,{},{},{:.6f},{:.6f},{}'.format(
                 epoch,
+                total_num_images_backpropped,
                 test_loss,
                 100.*correct/total,
                 time.time()))
-    '''
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    '''
 
 def main():
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
+    parser.add_argument('--batch-size', type=int, default=1, metavar='N',
+                        help='input batch size for training (default: 1)')
     parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                         help='input batch size for testing (default: 1000)')
     parser.add_argument('--epochs', type=int, default=500, metavar='N',
                         help='number of epochs to train (default: 10)')
     parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
                         help='learning rate (default: 0.01)')
+    parser.add_argument('--decay', default=0, type=float, help='decay')
     parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                         help='SGD momentum (default: 0.5)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
@@ -116,10 +191,18 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--top-k', type=int, default=1, metavar='N',
-                        help='how many images to backprop per batch')
+
     parser.add_argument('--selective-backprop', type=bool, default=False, metavar='N',
                         help='whether or not to use selective-backprop')
+    parser.add_argument('--top-k', type=int, default=8, metavar='N',
+                        help='how many images to backprop per batch')
+    parser.add_argument('--pool-size', type=int, default=16, metavar='N',
+                        help='how many images to backprop per batch')
+    parser.add_argument('--pickle-dir', default="/tmp/",
+                        help='directory for pickles')
+    parser.add_argument('--pickle-prefix', default="stats",
+                        help='file prefix for pickles')
+
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -128,13 +211,15 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
+    trainset = datasets.MNIST('../data', train=True, download=True,
                        transform=transforms.Compose([
                            transforms.ToTensor(),
                            transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
+                       ]))
+    trainset = [t + (i,) for i, t in enumerate(trainset)]           # Add image index to train set
+    chunk_size = args.pool_size * 10
+    partitions = [trainset[i:i + chunk_size] for i in xrange(0, len(trainset), chunk_size)]
+
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST('../data', train=False, transform=transforms.Compose([
                            transforms.ToTensor(),
@@ -142,13 +227,50 @@ def main():
                        ])),
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-
     model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.decay)
 
+    # Store frequency of each image getting backpropped
+    keys = range(len(trainset))
+    images_hist = dict(zip(keys, [0] * len(keys)))
+    batch_stats = []
+
+    # Make images hist pickle path
+    image_id_pickle_dir = os.path.join(args.pickle_dir, "image_id_hist")
+    if not os.path.exists(image_id_pickle_dir):
+        os.mkdir(image_id_pickle_dir)
+    image_id_pickle_file = os.path.join(image_id_pickle_dir,
+                                        "{}_images_hist.pickle".format(args.pickle_prefix))
+
+    # Make batch stats pickle path
+    batch_stats_pickle_dir = os.path.join(args.pickle_dir, "batch_stats")
+    if not os.path.exists(batch_stats_pickle_dir):
+        os.mkdir(batch_stats_pickle_dir)
+    batch_stats_pickle_file = os.path.join(batch_stats_pickle_dir,
+                                           "{}_batch_stats.pickle".format(args.pickle_prefix))
+
+    total_num_images_backpropped = 0
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader, epoch)
+        for partition in partitions:
+            trainloader = torch.utils.data.DataLoader(partition, batch_size=args.batch_size, shuffle=True, num_workers=2)
+            test(args, model, device, test_loader, epoch, total_num_images_backpropped)
+            num_images_backpropped = train(args,
+                                           model,
+                                           device,
+                                           trainloader,
+                                           optimizer,
+                                           epoch,
+                                           total_num_images_backpropped,
+                                           images_hist,
+                                           batch_stats=batch_stats)
+            total_num_images_backpropped += num_images_backpropped
+
+            with open(image_id_pickle_file, "wb") as handle:
+                pickle.dump(images_hist, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+            with open(batch_stats_pickle_file, "wb") as handle:
+                print(batch_stats_pickle_file)
+                pickle.dump(batch_stats, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == '__main__':
--- scripts/run_mnist.sh
+++ scripts/run_mnist.sh
@@ -2,15 +2,30 @@ set -x
 
 EXP_NAME=$1
 TOP_K=$2
-BATCH_SIZE=$3
+POOL_SIZE=$3
 LR=$4
+DECAY=0
 
 OUTPUT_DIR="/proj/BigLearning/ahjiang/output/mnist/"$EXP_NAME
-mkdir $OUTPUT_DIR
-OUTPUT_FILE="mnist_lecunn_"$TOP_K"_"$BATCH_SIZE"_"$LR".v0"
+OUTPUT_FILE="mnist_lecunn_"$TOP_K"_"$POOL_SIZE"_"$LR"_"$DECAY"_v1"
+PICKLE_DIR=$OUTPUT_DIR/pickles
 
-python mnist/main.py \
-  --batch-size $BATCH_SIZE \
-  --selective-backprop=True \
-  --top-k $TOP_K \
-  --lr $LR &> $OUTPUT_DIR/$OUTPUT_FILE
+mkdir $OUTPUT_DIR
+mkdir $PICKLE_DIR
+
+NUM_TRIALS=1
+for i in `seq 1 $NUM_TRIALS`
+do
+  OUTPUT_FILE="mnist_lecunn_"$TOP_K"_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i"_v1"
+  PICKLE_PREFIX="mnist_lecunn_"$TOP_K"_"$POOL_SIZE"_"$LR"_"$DECAY"_trial"$i
+
+  python mnist/main.py \
+    --batch-size 1 \
+    --selective-backprop=True \
+    --top-k $TOP_K \
+    --pool-size $POOL_SIZE \
+    --decay $DECAY \
+    --pickle-dir=$PICKLE_DIR \
+    --pickle-prefix=$PICKLE_PREFIX \
+    --lr $LR &> $OUTPUT_DIR/$OUTPUT_FILE
+done
